62da8767de53bd692484c82a69cf489bee0a3756
Efderosnay <e.fderosnay@gmail.com>
2019-07-03 17:04:41 +0100

    with input

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ let rowData = []
 let headerTime = document.getElementById('headerTime')
 let minutes = 20
 let gridArray =[]
-let nameInput
-let reasonInput
-let notesInput
+let nameInput = ''
+let reasonInput = ''
+let notesInput = ''
+
 headerTime.addEventListener('keypress',(e)=>{
     if(e.code === 'Enter'){
     minutes = Number(document.getElementById('headerTime').value)
+    headerTime.value = ''
 
     //forloop to pop goes here
+    document.getElementById('myGrid').innerHTML =''
 
    makeGrid()
 
@@ -31,12 +34,15 @@ for(i=9;i<17;i++){
        let nameRow = document.createElement('div')
        nameRow.innerHTML = `${nameInput}`
        nameRow.className ='rowSpacing'
+    //    nameRow.id = `nameRowID${i}${h}`
        let reasonRow = document.createElement('div')
        reasonRow.innerHTML = `${reasonInput}`
        reasonRow.className = 'rowSpacing'
+    //    reasonRow.id = `reasonRowID${i}${h}`
        let notesRow = document.createElement('div')
        notesRow.innerHTML = `${notesInput}` 
        notesRow.className = 'rowSpacing' 
+    //    notesRow.id = `notesRowID${i}${h}`
         let row = document.createElement('div')
         row.className = "rowClass"
         row.appendChild(timeRow)
@@ -44,12 +50,50 @@ for(i=9;i<17;i++){
         row.appendChild(reasonRow)
         row.appendChild(notesRow)
         let div = document.createElement("div")
-        div.id = `id${i}${h}`
+        div.id = `${i}:${h}`
+        //popup command 
+        div.ondblclick = () => {
+            console.log(div)
+            document.getElementById('myModal').style.visibility = 'visible'
+            
+
+            document.getElementById('submitButton').addEventListener('click',()=>{
+                console.log('test')
+                modal.style.visibility = "hidden"
+                let firstname = document.getElementById('firstname').value
+                let surname = document.getElementById('surname').value
+                let reason = document.getElementById('reason').value
+                let notes = document.getElementById('notes').value
+
+                // console.log(div.innerHTML)
+
+              
+                
+
+                // document.getElementById(`nameRowID${i}${h}`).innerHTML = `${surname},${firstname}`
+                // document.getElementById(`reasonRowID${i}${h}`).innerHTML = reason
+                // document.getElementById(`notesRowID${i}${h}`).innerHTML = notes
+
+                div.innerHTML = `<div class="rowClass"><div class="rowSpacing">${div.id}</div><div class="rowSpacing">${firstname + ' ' + surname}</div><div class="rowSpacing">${reason}</div><div class="rowSpacing">${notes}</div></div>`
+
+                firstname.value = ''
+                surname.value = ''
+                reason.value = ''
+                notes.value = ''
+            //   console.log(div.id)
+            })
+          
+
+
+
+        }
+
+
         div.appendChild(row)
         document.getElementById("myGrid").appendChild(div)
         // row.id= `row${i}`
         rowData.push(row)
-        console.log("working")
+        // console.log("working")
         if(h=='00'){
             h=0
         }else if(h=='05'){
@@ -59,3 +103,15 @@ for(i=9;i<17;i++){
     }
 }
 }
+
+var modal = document.getElementById("myModal");
+var span = document.getElementsByClassName("close")[0];
+span.onclick = function() {
+    modal.style.visibility = "hidden";
+  }
+
+
+
+  let updateGrid = () => {
+
+  }
